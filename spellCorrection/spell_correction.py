688cbcf2e5d7b0d4a460d9be296714735748e1fe
Carlos GIL <lao.tseu.is.alive@gmail.com>
2024-12-18 16:04:11 +0100

    counting differences

--- spellCorrection/spell_correction.py
+++ spellCorrection/spell_correction.py
@@ -1,4 +1,6 @@
 import re
+
+from nltk.translate.bleu_score import modified_precision
 from spellchecker import SpellChecker
 import hunspell
 import language_tool_python
@@ -9,51 +11,89 @@ from rich.text import Text
 console = Console()
 
 
-def highlight_differences(prepend_msg, false_phrase, correct_phrase, style_ok="green", style_ko="bold red"):
+def highlight_differences(prepend_msg, original, modified, style_ok="green", style_replace="bold red", style_insert="bold magenta", style_delete="bold strike yellow"):
     # Supprimer les espaces pour la comparaison
-    false_phrase_no_spaces = false_phrase.replace(" ,", ",")
-    correct_phrase_no_spaces = correct_phrase.replace(" ,", ",")
+    original_no_spaces = original.replace(" ,", ",")
+    modified_no_spaces = modified.replace(" ,", ",")
 
-    matcher = SequenceMatcher(None, false_phrase_no_spaces, correct_phrase_no_spaces)
+    matcher = SequenceMatcher(None, original_no_spaces, modified_no_spaces)
     highlighted_text = Text()
     highlighted_text.append(prepend_msg , style="white")
+
 
     # Comparaison caractère par caractère
     i_false = 0
     i_correct = 0
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        false_chunk = false_phrase_no_spaces[i1:i2]
-        correct_chunk = correct_phrase_no_spaces[j1:j2]
+        original_chunk = original_no_spaces[i1:i2]
+        modified_chunk = modified_no_spaces[j1:j2]
 
         if tag == 'equal':
             # Partie correcte (vert)
-            highlighted_text.append(false_chunk, style=style_ok)
+            highlighted_text.append(original_chunk, style=style_ok)
         elif tag == 'replace':
             # Partie incorrecte remplacée (rouge gras)
-            highlighted_text.append(false_chunk, style=style_ko)
+            highlighted_text.append(modified_chunk, style=style_replace)
         elif tag == 'delete':
             # Partie supprimée (rouge gras)
-            highlighted_text.append(false_chunk, style=style_ko)
+            highlighted_text.append(original_chunk, style=style_delete)
         elif tag == 'insert':
             # Partie ajoutée (on la saute dans la phrase incorrecte)
-            pass
+            highlighted_text.append(modified_chunk, style=style_insert)
 
         # Met à jour les index dans les phrases d'origine
-        i_false += len(false_chunk)
-        i_correct += len(correct_chunk)
+        i_false += len(original_chunk)
+        i_correct += len(modified_chunk)
+
 
     return highlighted_text
 
-def compare_phrases(false_phrase, correct_phrase):
-    matcher = SequenceMatcher(None, false_phrase, correct_phrase)
+def count_differences(original, modified):
+    matcher = SequenceMatcher(None, original, modified)
+    count_equal = 0
+    count_replace = 0
+    count_insert = 0
+    count_delete = 0
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        original_chunk = original[i1:i2]
+        modified_chunk = modified[j1:j2]
+        if tag == 'equal':
+            count_equal += len(original_chunk)
+        elif tag == 'replace':
+            count_replace += len(original_chunk)
+        elif tag == 'insert':
+            count_insert += len(modified_chunk)
+        elif tag == 'delete':
+            count_delete += len(original_chunk)
+        else:
+            raise ValueError(f"Opération inconnue : {tag}")
+
+    return { 'equal': count_equal, 'replace': count_replace, 'insert': count_insert, 'delete': count_delete }
+
+def get_highlighted_differences_counter(prepend_msg, original_phrase, modified_phrase, style_ok="green", style_replace="bold red", style_insert="bold magenta", style_delete="bold yellow"):
+    counters=count_differences(original_phrase, modified_phrase)
+    highlighted_text = Text()
+    highlighted_text.append(prepend_msg, style="white")
+    highlighted_text.append(f"equal : {counters['equal']}, ", style=style_ok)
+    highlighted_text.append(f"replace : {counters['replace']}, ", style=style_replace)
+    highlighted_text.append(f"insert : {counters['insert']}, ", style=style_insert)
+    highlighted_text.append(f"delete : {counters['delete']}", style=style_delete)
+    return highlighted_text
+
+def compare_phrases(original, modified):
+    matcher = SequenceMatcher(None, original, modified)
     differences = []
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        if tag != 'equal':
-            differences.append({
-                'operation': tag,
-                'false_text': false_phrase[i1:i2],
-                'correct_text': correct_phrase[j1:j2]
-            })
+        #if tag != 'equal':
+        differences.append({
+            'operation': tag,
+            'original_text': original[i1:i2],
+            'modified_text': modified[j1:j2],
+            'original_start': i1,
+            'original_end': i2,
+            'modified_start': j1,
+            'modified_end': j2
+        })
     return differences
 
 
@@ -136,20 +176,28 @@ def corriger_orthographe_03(texte):
 
 if __name__ == '__main__':
 
+    GoDEBUG = False
     # Exemple d'utilisation
-    texte_faux = "Voici un exenple de texte, de l'auteur François Martin, avic moins de 24 fotes d'ortographe."
+    texte_faux =    "Voici un exenple de texte, de l'auteur François Martin, avic moins de 24 fotes d'ortographe."
     texte_correct = "Voici un exemple de texte, de l'auteur François Martin, avec moins de 24 fautes d'orthographe."
-    console.print(highlight_differences("Texte faux :\t",texte_faux, texte_correct, style_ok="white"))
+    console.print(highlight_differences("Texte ok   :\t",texte_correct, texte_correct, style_ok="bold white"))
+    console.print(get_highlighted_differences_counter("Texte ok   :\t", texte_correct, texte_correct, style_ok="bold white"))
+    console.print(highlight_differences("Texte faux :\t",texte_correct, texte_faux , style_ok="white"))
+    console.print(get_highlighted_differences_counter("Texte faux :\t", texte_correct, texte_faux, style_ok="white"))
     texte_corrige_01 = corriger_orthographe_01(texte_faux)
-    console.print(highlight_differences("spellchecker :\t",texte_corrige_01, texte_correct))
-    # different_words = compare_phrases(texte_faux, texte_corrige_01)
-    # for diff in different_words:
-    #   print(f"Opération : {diff['operation']}")
-    #   print(f"Texte incorrect : {diff['false_text']}")
-    #   print(f"Texte correct : {diff['correct_text']}")
-    #   print(diff)
+    console.print(highlight_differences("spellchecker :\t", texte_correct,texte_corrige_01))
+    console.print(get_highlighted_differences_counter("spellchecker :\t", texte_correct, texte_corrige_01))
+    if GoDEBUG:
+        different_words = compare_phrases(texte_corrige_01, texte_correct)
+        for diff in different_words:
+          print(f"Opération : {diff['operation']}")
+          print(f"Texte incorrect : {diff['original_text']}")
+          print(f"Texte correct : {diff['modified_text']}")
+          print(diff)
 
     texte_corrige_02 = corriger_orthographe_02(texte_faux)
-    console.print(highlight_differences(" hunspell    :\t", texte_corrige_02, texte_correct))
+    console.print(highlight_differences(" hunspell    :\t", texte_correct, texte_corrige_02))
+    console.print(get_highlighted_differences_counter(" hunspell    :\t", texte_correct, texte_corrige_02))
     texte_corrige_03 = corriger_orthographe_03(texte_faux)
-    console.print(highlight_differences("LanguageTool :\t", texte_corrige_03, texte_correct))
+    console.print(highlight_differences("LanguageTool :\t", texte_correct, texte_corrige_03, ))
+    console.print(get_highlighted_differences_counter("LanguageTool :\t", texte_correct, texte_corrige_03))
