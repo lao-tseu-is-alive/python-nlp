f73a4a3fbcef3ff95d2ad3917ee21fe60be4b0dc
Carlos GIL <lao.tseu.is.alive@gmail.com>
2024-12-18 11:46:05 +0100

    added language_tool round

--- spellCorrection/spell_correction.py
+++ spellCorrection/spell_correction.py
@@ -1,6 +1,8 @@
 import re
 from spellchecker import SpellChecker
 import hunspell
+import language_tool_python
+from difflib import SequenceMatcher
 
 
 def corriger_orthographe_01(texte):
@@ -13,6 +15,9 @@ def corriger_orthographe_01(texte):
   Returns:
     Le texte corrigé.
   """
+    # Initialiser le correcteur orthographique spellchecker
+    spell = SpellChecker(language='fr')
+
     # Séparer les mots et la ponctuation
     mots = re.findall(r'\w+|[^\w\s]', texte, re.UNICODE)
 
@@ -20,9 +25,9 @@ def corriger_orthographe_01(texte):
     mots_corriges = []
     for mot in mots:
       # Vérifier si le mot est mal orthographié
-      if mot not in spell01:
+      if mot not in spell:
         # Obtenir la correction
-        correction = spell01.correction(mot)
+        correction = spell.correction(mot)
         mots_corriges.append(correction)
       else:
         mots_corriges.append(mot)
@@ -41,11 +46,14 @@ def corriger_orthographe_02(texte):
   Returns:
       Le texte corrigé.
   """
+  # Initialize the Hunspell spell checker for French
+  spell = hunspell.HunSpell('/usr/share/hunspell/fr.dic', '/usr/share/hunspell/fr.aff')
+
   mots = texte.split()
   mots_corriges = []
   for mot in mots:
-    if not spell02.spell(mot):
-      suggestions = spell02.suggest(mot)
+    if not spell.spell(mot):
+      suggestions = spell.suggest(mot)
       if suggestions:
         mots_corriges.append(suggestions[0])
       else:
@@ -55,17 +63,49 @@ def corriger_orthographe_02(texte):
   return ' '.join(mots_corriges)
 
 
-# Initialiser le correcteur orthographique spellchecker
-spell01 = SpellChecker(language='fr')
+def corriger_orthographe_03(texte):
+  """
+  Corrige les fautes d'orthographe dans un texte en français avec language_tool_python.
 
-# Initialize the Hunspell spell checker for French
-spell02 = hunspell.HunSpell('/usr/share/hunspell/fr.dic', '/usr/share/hunspell/fr.aff')
+  Args:
+      texte: Le texte à corriger.
+
+  Returns:
+      Le texte corrigé.
+  """
+  # Initialize the LanguageTool spell checker for French
+  spell = language_tool_python.LanguageTool('fr-FR')
+  matches = spell.check(texte)
+  texte_corrige = language_tool_python.utils.correct(texte, matches)
+  return texte_corrige
+
+def compare_phrases(false_phrase, correct_phrase):
+    matcher = SequenceMatcher(None, false_phrase, correct_phrase)
+    differences = []
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        if tag != 'equal':
+            differences.append({
+                'operation': tag,
+                'false_text': false_phrase[i1:i2],
+                'correct_text': correct_phrase[j1:j2]
+            })
+    return differences
 
 
 # Exemple d'utilisation
-texte_faux = "Ceci est un exenple de texte avic moins de 2004 fotes d'ortographe."
+texte_faux = "Voici un exenple de texte, de l'auteur François Martin avic moins de 2004 fotes d'ortographe."
 print(f"Texte faux   :\t {texte_faux}")
 texte_corrige_01 = corriger_orthographe_01(texte_faux)
 print(f"spellchecker :\t {texte_corrige_01}")
+
+differences = compare_phrases(texte_faux, texte_corrige_01)
+for diff in differences:
+  print(f"Opération : {diff['operation']}")
+  print(f"Texte incorrect : {diff['false_text']}")
+  print(f"Texte correct : {diff['correct_text']}")
+  print(diff)
+
 texte_corrige_02 = corriger_orthographe_02(texte_faux)
 print(f" hunspell    :\t {texte_corrige_02}")
+texte_corrige_03 = corriger_orthographe_03(texte_faux)
+print(f"LanguageTool :\t {texte_corrige_03}")
